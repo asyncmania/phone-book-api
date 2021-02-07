f48fb0b833b940b70cacf1137618cce9ab5bd2a7
jilesanmi <ilesanmi.josiah@gmail.com>
2021-02-07 18:20:10 +0100

    addind contact tests

--- __tests__/contactsRoutes.ts
+++ __tests__/contactsRoutes.ts
@@ -1,11 +1,48 @@
 
+import request from 'supertest'
+import sequelize from '../src/loaders/db';
+import { IContact } from '../src/models/contact';
+import { contacts } from '../src/__mocks__/mockContactsRepository';
+import { users } from '../src/__mocks__/mockUsersRepository';
 
+
+
+
+
+const app = `http://localhost:3600`;
+
+beforeEach(async () => {
+ await sequelize.query(`DELETE FROM contacts`, null)
+})
+
+afterAll(() => sequelize.close())
 
 
 describe('Contact Routes', () => {
 
-  test('Add Contact', () => {
+  test('reaching any contact without credentials should return 401', async () => {
     
+    await request(app)
+      .post('/api/contacts')
+      .expect(401)
+
   })
+
+
+  test('Adding a contact with credentials', async () => {
+
+    const contact: IContact = contacts[0]
+    const token = Buffer.from(`${users[0].email}:${users[0].password}`).toString("base64")
+     const response = await request(app)
+     .post('/api/contacts')
+     .set('Authorization', `Basic ${token}`)
+     .send(contact)
+     .expect(201)
+     .expect('Content-Type', /json/)
+
+
+     expect(response.body).toMatchObject(contact)
+  })
+
 
 })
